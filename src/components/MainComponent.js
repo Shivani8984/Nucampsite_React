61237cae8f23719bb0d1d0c56a00f5d3744d0fa1
Shivani8984 <shivani8984@gmail.com>
2020-10-23 14:56:50 -0400

    indented

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-//import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
-import Contact from './ContactComponent';
+
 
 class Main extends Component {
     constructor(props) {
