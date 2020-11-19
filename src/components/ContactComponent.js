0390464a133f814a36bb20cffe97f8f70c224366
Shivani8984 <shivani8984@gmail.com>
2020-11-19 16:11:24 -0500

    final changes

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -34,10 +34,9 @@ class Contact extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+    handleSubmit(values) {        
         this.props.resetFeedbackForm();
+        this.props.postFeedback(values);
     }
 
     render() {
